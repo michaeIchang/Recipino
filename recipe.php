0ebff3a0f6057bec96797ade9b2c9e02b983f893
Empiezan <michaelchang@wustl.edu>
2018-02-28 01:48:08 -0600

    recipe ingredients and steps now converted to json

--- recipe.php
+++ recipe.php
@@ -1,12 +1,55 @@
 <?php
 $recipe = $_POST['recipeSteps'];
 
-$myfile = fopen("recipe.html", "w") or die("Unable to open file!");
-fwrite($myfile, $recipe);
-fclose($myfile);
+//$myfile = fopen("recipe.html", "w") or die("Unable to open file!");
+//fwrite($myfile, $recipe);
+//fclose($myfile);
 
-$arr = array("steps" => $recipe);
+//$recipeWords = str_split($recipe);
+//
+//$readIngredients = false;
+//$readSteps = false;
 
-echo $arr;
+//$ingredients = array();
+//$steps = array();
+
+
+function isEmpty($var){
+    return empty($var);
+}
+
+$recipeArr = explode("Ingredients", $recipe);
+$recipeArr = explode("Method", $recipeArr[1]);
+//$recipeArr = json_encode($recipeArr);
+$ingredients = explode("\n", $recipeArr[0]);
+$steps = explode("\n", $recipeArr[1]);
+
+
+
+for ($i = 0; $i < count($ingredients); $i++) {
+    $ingredients[$i] = str_replace("\r", '', $ingredients[$i]);
+//    $ingredients[$i] = str_replace("\\", '', $ingredients[$i]);
+
+}
+
+for ($i = 0; $i < count($steps); $i++) {
+    $steps[$i] = str_replace("\r", '', $steps[$i]);
+//    $steps[$i] = str_replace("\\", '', $steps[$i]);
+}
+
+$ingredients = array_filter($ingredients);
+$steps = array_filter($steps);
+
+$arr["Ingredients"] = $ingredients;
+$arr["Steps"] = $steps;
+
+//$fp = fopen('recipe.json', 'w');
+//fwrite($fp, json_encode($arr));
+//fclose($fp)
+
+file_put_contents('recipe.json', json_encode($arr));
+
+echo json_encode($arr);
+//header("recipe.json");
 
 ?>
